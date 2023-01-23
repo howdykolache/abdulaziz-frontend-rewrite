a5b7af0148cba0f61480a16301749b45fbb00fdd
Yassin Youssoufm <yassinrsc@gmail.com>
2023-01-23 17:13:16 +0300

    Typo

--- utils/pdfs.js
+++ utils/pdfs.js
@@ -117,8 +117,8 @@ const displayOrderMetadata = (order) => {
             let value = 'n\\a'
             
             if (order.contactName) value = order.contactName
-            if (order.client.email) value += `$\n${order.client.email}`
-            if (order.phoneNumber) value += `$\n${order.phoneNumber}`
+            if (order.client.email) value += `\n${order.client.email}`
+            if (order.phoneNumber) value += `\n${order.phoneNumber}`
 
             return value
           },
